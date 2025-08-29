88eee8316dcd3e20d671b22843efa9947e551d1e
MD Saifur Rahaman <asifforizy@gmail.com>
2025-08-29 16:08:00 +0600

    call and hearts functionality added

---
+++ script.js
@@ -0,0 +1,170 @@
+document.getElementById("addlove1").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove2").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove3").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove4").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove5").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove6").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove7").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove8").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+document.getElementById("addlove9").addEventListener("click", function(event) {
+    event.preventDefault();
+    const loveCount = document.getElementById("love-count");
+    loveCount.innerText = parseInt(loveCount.innerText) + 1;
+});
+
+
+document.getElementById("call1").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call2").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+
+document.getElementById("call3").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call4").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call5").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call6").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call7").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+
+document.getElementById("call8").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
+
+document.getElementById("call9").addEventListener("click", function(event) {
+    event.preventDefault();
+    const coinCount = document.getElementById("coinCount");
+    if (parseInt(coinCount.innerText) > 0) {
+        coinCount.innerText = parseInt(coinCount.innerText) - 20;
+        alert("Calling National Emergency Service 999");
+    }
+    else {
+        alert("Not enough coins to make a call you need at least 20 coins.");
+    }
+});
